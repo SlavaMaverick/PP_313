70c6d4c4446421ffe6948f46bc2de642b92b575b
slava <slavamaverick@yandex.ru>
2022-09-23 13:42:49 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,37 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import javax.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+
+    // или заменить всё, что ниже на lombok ?
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
